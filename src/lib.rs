3284aff8a4962f37e707648ed504bff641e5026c
Juliette Cordor <grassojames5@gmail.com>
2022-08-25 11:13:58 +1000

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,12 @@ use quote::quote;
 fn dotenv_inner(item: TokenStream) -> TokenStream {
     let item_str = item.to_string();
 
-    let item_path = quote! {
-        "../#item_str"
-    };
+    let path = format!("../{}", item_str);
+
+    let path_lit = litrs::StringLit::parse(path).unwrap();
 
     quote! {
-        fn your_mother() -> &'static str { #item_path }
+        fn your_mother() -> &'static str { #path_lit }
     }
 }
 
