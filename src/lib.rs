7f568118093bee24696336b207a759e35aacd22f
Juliette Cordor <grassojames5@gmail.com>
2022-08-25 12:46:44 +1000

    fixed issues

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro2::TokenStream;
-use quote::quote;
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
 
 fn dotenv_inner(item: TokenStream) -> TokenStream {
     let item_str = {
@@ -14,29 +15,37 @@ fn dotenv_inner(item: TokenStream) -> TokenStream {
 
     let path = std::env::current_dir().unwrap().join(item_str);
 
-    let path_lit = litrs::StringLit::parse(path).unwrap();
-    let path_lit_val = path_lit.value();
+    let item_span = item.span();
+
+    if !path.exists() {
+        return quote_spanned! {
+            item_span => compile_error!("Env file does not exists")
+        };
+    }
+
+    // let path_lit = litrs::StringLit::parse(path).unwrap();
+    // let path_lit_val = path_lit.value();
 
     quote! {
         {
-            const ENV_FILE: &str = include_str!(#path_lit_val);
+            // const ENV_FILE: &str = include_str!(#path_lit_val);
 
-            for line in ENV_FILE.lines() {
-                let mut var = line.split('=');
+            // for line in ENV_FILE.lines() {
+            //     let mut var = line.split('=');
 
-                let decl_opt = var.next();
-                let value_opt = var.next();
+            //     let decl_opt = var.next();
+            //     let value_opt = var.next();
 
-                if let Some(decl) = decl_opt {
-                    if let Some(value) = value_opt {
-                        if decl.contains(" ") || value.contains(" ") {
-                            panic!("Invalid .env file")
-                        }
+            //     if let Some(decl) = decl_opt {
+            //         if let Some(value) = value_opt {
+            //             if decl.contains(" ") || value.contains(" ") {
+            //                 panic!("Invalid .env file")
+            //             }
 
-                        std::env::set_var(decl, value);
-                    }
-                }
-            }
+            //             std::env::set_var(decl, value);
+            //         }
+            //     }
+            // }
         }
     }
 }
