9f9521dab1664d7d5772975269b2da4d826a454d
Juliette Cordor <grassojames5@gmail.com>
2022-08-25 11:06:15 +1000

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use quote::quote;
+
+fn dotenv_inner(item: TokenStream) -> TokenStream {
+    let item_str = item.to_string();
+    let current_dir = std::env::current_dir().unwrap();
+
+    let item_path = quote! {};
+
+    TokenStream::new()
+}
 
 #[proc_macro]
-pub fn dotenv(item: TokenStream) -> TokenStream {
-    TokenStream::new()
+pub fn dotenv(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    dotenv_inner(item.into()).into()
 }
