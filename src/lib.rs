ed8a6007155ebecdc72e59a12088ce2ad543bd52
Juliette Cordor <grassojames5@gmail.com>
2022-08-25 13:15:59 +1000

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -82,8 +82,7 @@ fn dotenv_inner(item: TokenStream) -> TokenStream {
 
     quote! {
         {
-            [#(#var_vec_tokens),*]
-            // const ENV_FILE: [(String, String); #var_vec_len] = [#(#var_vec),*];
+            const ENV_FILE: [(String, String); #var_vec_len] = [#(#var_vec_tokens),*];
 
             // for line in ENV_FILE.lines() {
             //     let mut var = line.split('=');
